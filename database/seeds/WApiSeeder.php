0b697e9d8c4e4fba93745ca6adc8b2487abc24ba
wangxiao <wx497657341@qq.com>
2018-09-28 15:10:30 +0800

    system base data seeder

---
+++ database/seeds/WApiSeeder.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangxiao
+ * Date: 2018/9/28
+ * Time: 14:52
+ */
+
+use Illuminate\Database\Seeder;
+
+class WApiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $systemMenuId = $this->system();
+        $this->menu($systemMenuId);
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function system()
+    {
+        $systemMenu                  = new \App\Entities\AdminMenu();
+        $systemMenu->name            = '系统管理';
+        $systemMenu->permission_name = 'admin.systems.index';
+        $systemMenu->parent_id       = 0;
+        $systemMenu->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.systems.index';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $systemMenu->id;
+        $permission->save();
+
+        return $systemMenu->id;
+    }
+
+    /**
+     * @param $parentId
+     */
+    public function menu($parentId)
+    {
+        $menu                  = new \App\Entities\AdminMenu();
+        $menu->name            = '后台菜单';
+        $menu->permission_name = 'admin.admin-menus.index';
+        $menu->parent_id       = $parentId->id;
+        $menu->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.admin-menus.index';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.admin-menus.create';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.admin-menus.update';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.admin-menus.delete';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+    }
+
+    /**
+     * @param $parentId
+     */
+    public function permission($parentId)
+    {
+        $menu                  = new \App\Entities\AdminMenu();
+        $menu->name            = '权限';
+        $menu->permission_name = 'admin.permissions.index';
+        $menu->parent_id       = $parentId->id;
+        $menu->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.permissions.index';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.permissions.create';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.permissions.update';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.permissions.delete';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+    }
+
+    /**
+     * @param $parentId
+     */
+    public function role($parentId)
+    {
+        $menu                  = new \App\Entities\AdminMenu();
+        $menu->name            = '角色';
+        $menu->permission_name = 'admin.roles.index';
+        $menu->parent_id       = $parentId->id;
+        $menu->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.roles.index';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.roles.create';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.roles.update';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+
+        $permission             = new \App\Entities\Permission();
+        $permission->name       = 'admin.roles.delete';
+        $permission->guard_name = 'admin';
+        $permission->menu_id    = $menu->id;
+        $permission->save();
+    }
+}
